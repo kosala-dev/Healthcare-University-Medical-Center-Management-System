fccc6f0ec2290f3626392b21acb9f99c222699e2
sahani02 <2002sahani@gmail.com>
2025-11-16 17:01:07 +0530

    Add files via upload

---
+++ Upload Here/notificationRoute.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const Notification = require("../models/Notification");
+
+router.get("/", async (req, res) => {
+  const notifications = await Notification.find().sort({ date: -1 });
+  res.json(notifications);
+});
+
+module.exports = router;
